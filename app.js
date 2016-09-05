1bfa3eb3467f5e2b7b34476238b78f0f3e67559b
ivan huay <ivanhuay@gmail.com>
2016-09-05 15:26:03 -0300

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+var mnist = require("mnist");
+var synaptic = require("synaptic");
+
+var set = mnist.set(700, 20);
+
+var trainingSet = set.training;
+var testSet = set.test;
+
+
+var Layer = synaptic.Layer;
+var Network = synaptic.Network;
+var Trainer = synaptic.Trainer;
+
+var inputLayer = new Layer(784);
+var hiddenLayer = new Layer(100);
+var outputLayer = new Layer(10);
+
+
+
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+var myNetwork = new Network({
+    input: inputLayer,
+    hidden: [hiddenLayer],
+    output: outputLayer
+});
+
+
+
+var trainer = new Trainer(myNetwork);
+trainer.train(trainingSet, {
+    rate: 0.2,
+    iterations: 20,
+    error: 0.1,
+    shuffle: true,
+    log: 1,
+    cost: Trainer.cost.CROSS_ENTROPY
+});
+
+
+
+console.log(myNetwork.activate(testSet[0].input));
+console.log(testSet[0].output);
